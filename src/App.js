e0b6cc85bf3df2f11ab8780e986a6461f8b48dc4
Mounika <yogamounikametla@gmail.com>
2022-10-15 15:55:33 +0000

    source

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import MatchGame from './Components/Match Game'
+import './App.css'
+
+const App = () => <MatchGame />
+
+export default App
---
+++ src/Components/Match Game/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+import GameStructure from '../Game Structure'
+
+class MatchGames extends Component {
+  render() {
+    return (
+      <div>
+        <GameStructure />
+      </div>
+    )
+  }
+}
+
+export default MatchGames
